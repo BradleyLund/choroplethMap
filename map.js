8b61d67976b0c803739d066242de798d50932f78
Bradley Lund <bradjlund@gmail.com>
2020-11-13 22:05:51 +0200

    ugh

--- map.js
+++ map.js
@@ -1,7 +1,39 @@
-fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
-  .then(response => response.json())
-  .then(data=> {
+var width = 720;
+var height = 500;
 
+var projection = d3.geoAlbers()
+    .scale(1000)
+    .translate([width / 2, height/2]);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("heightd",height);
+
+
+
+
+
+d3.queue()
+    .defer(d3.json,'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json')
+    .await(ready)
+
+
+    function ready(error,us) {
+
+        
+        console.log(us.objects.counties)
+        if (error) throw error;
     
+        svg.append("g")
+            .attr("class", "counties")
+            .selectAll("path")
+                .data(topojson.feature(us,us.objects.counties).features)
+            .enter().append("path")
+                .attr("d",path)
+                .style("fill","white")
+                .style("stroke","black")
+    }
 
-  })
